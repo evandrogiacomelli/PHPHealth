867f898cafce82e10df94a90e3a1fd0505a220df
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2026-01-12 10:29:09 -0300

    patient root

---
+++ src/domain/patient/entity/Patient.php
@@ -0,0 +1,30 @@
+<?php
+
+class Patient
+{
+    private PatientInfo $info;
+    private PatientAddress $address;
+    private PatientContact $contact;
+
+    private function __construct
+    (
+        PatientInfo $info,
+        PatientAddress $address,
+        PatientContact $contact
+    )
+    {
+        $this->info = $info;
+        $this->address = $address;
+        $this->contact = $contact;
+    }
+
+    public static function create(
+        PatientInfo $info,
+        PatientAddress $address,
+        PatientContact $contact
+
+    ): self {
+        return new self($info, $address, $contact);
+    }
+
+}
