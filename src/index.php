d93f9eaecc8867bd7484ff97c48e609a875dc371
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2026-01-12 15:14:58 -0300

    server router + index

--- src/index.php
+++ src/index.php
@@ -5,4 +5,21 @@ header('Content-Type: application/json');
 
 date_default_timezone_set('America/Sao_Paulo');
 
-var_dump($_GET['path']);
+//var_dump($_GET['path']);
+
+//$path = explode('/', $_GET['path']);
+
+if (isset($_GET['path'])) {
+    $path = explode('/', $_GET['path']);
+} else {
+    echo "caminho não existe"; exit;
+}
+
+if (isset($path[0])) { $api = $path[0]; }  else { echo "caminho nao existe" ; exit; }
+if (isset($path[1])) { $acao = $path[1]; }  else { $acao = ''; }
+if (isset($path[2])) { $param = $path[2]; }  else { $param = ''; }
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+var_dump($method);
