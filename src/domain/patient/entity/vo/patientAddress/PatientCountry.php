8946b1f401d34fc82148ac8b6f476aba7f321446
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2026-01-12 12:45:12 -0300

    PatientCountry, vo

---
+++ src/domain/patient/entity/vo/patientAddress/PatientCountry.php
@@ -0,0 +1,26 @@
+<?php
+
+class PatientCountry
+{
+    private string $value;
+
+    private function __construct(string $value) {
+        $this->value = $value;
+    }
+
+    public static function fromString(string $value): self {
+        self::validate($value);
+        return new self($value);
+    }
+
+    // melhorar validação
+    private static function validate(string $value): void {
+        if ($value === '') {
+            throw new \InvalidArgumentException('Invalid country');
+        }
+    }
+
+    public function toString(): string {
+        return $this->value;
+    }
+}
