56055a580b886fd9d359da29d2c4035391efffdf
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2026-01-12 12:39:54 -0300

    PatientPostalCode, vo

---
+++ src/domain/patient/entity/vo/patientAddress/PatientPostalCode.php
@@ -0,0 +1,31 @@
+<?php
+
+class PatientPostalCode
+{
+    private string $value;
+
+    private function __construct(string $value) {
+        $this->value = $value;
+    }
+
+    public static function create(string $value): self {
+        $normalized = self::normalize($value);
+        self::validate($normalized);
+
+        return new self($normalized);
+    }
+
+    private static function normalize(string $value): string {
+        return preg_replace('/\D/', '', $value);
+    }
+
+    private static function validate(string $value): void {
+        if (strlen($value) !== 8) {
+            throw new \InvalidArgumentException("Postal code must be 8 digits long");
+        }
+    }
+
+    public function toString(): string {
+        return $this->value;
+    }
+}
